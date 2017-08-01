d65ceacf546c5678cc270740aee5067fe363cde4
josh <josh.cannons@gmail.com>
2017-08-01 13:21:48 +1000

    initial

--- website/settings.py
+++ website/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'taggit',
     'music.apps.MusicConfig',
-    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
