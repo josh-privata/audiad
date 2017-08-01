d8735e6ce066a964b602bb1d834cdc93744460ce
josh <josh.cannons@gmail.com>
2017-08-01 13:56:31 +1000

    initial

--- website/settings.py
+++ website/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'taggit',
+    'blog.apps.BlogConfig',
     'music.apps.MusicConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- website/urls.py
+++ website/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^blog/', include('blog.urls')),
     url(r'^music/', include('music.urls')),
     url(r'^admin/', admin.site.urls),
 
