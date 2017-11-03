e275a013b63bd2369bddff84a2ba1924032e0b0a
Josh Cannons <josh.cannons@gmail.com>
2017-11-04 08:59:03 +1000

    travis fix commente

--- music/tests.py
+++ music/tests.py
@@ -3,246 +3,246 @@
 
 Example:
 
-
-Attributes:
-    module_level_variable1 (int): Module level variables may be documented in
-        either the ``Attributes`` section of the module docstring, or in an
-        inline docstring immediately following the variable.
-
-Todo:
-
-"""
-# todo Finish docstring
-
-from django.core.urlresolvers import reverse
-from django.urls import resolve
-from django.test import TestCase
-from .views.views import *
-from .models import *
-
-
-class PageTests(TestCase):
-    """docstring
-
-        Properties created with the ``@property`` decorator should be documented
-        in the property's getter method.
-
-        Attributes:
-            attr1 (str): Description of `attr1`.
-            attr2 (:obj:`int`, optional): Description of `attr2`.
-
-        """
-    # todo Finish docstring
-
-    def setUp(self):
-        """docstring
-
-                Note:
-                    Do not include the `self` parameter in the ``Args`` section.
-
-                Args:
-                    param1: The first parameter.
-                    param2: The second parameter.
-
-                Returns:
-                    True if successful, False otherwise.
-
-                """
-        # todo Finish docstring
-        # Setup run before every test method.
-        pass
-
-    def tearDown(self):
-        """docstring
-
-                Note:
-                    Do not include the `self` parameter in the ``Args`` section.
-
-                Args:
-                    param1: The first parameter.
-                    param2: The second parameter.
-
-                Returns:
-                    True if successful, False otherwise.
-
-                """
-        # todo Finish docstring
-        # Clean up run after every test method.
-        pass
-
-    def test_view_url_exists_at_desired_location(self):
-        """docstring
-
-                Note:
-                    Do not include the `self` parameter in the ``Args`` section.
-
-                Args:
-                    param1: The first parameter.
-                    param2: The second parameter.
-
-                Returns:
-                    True if successful, False otherwise.
-
-                """
-        # todo Finish docstring
-        resp = self.client.get('/music')
-        self.assertEqual(resp.status_code, 200)
-
-    def test_view_url_accessible_by_name(self):
-        """docstring
-
-                Note:
-                    Do not include the `self` parameter in the ``Args`` section.
-
-                Args:
-                    param1: The first parameter.
-                    param2: The second parameter.
-
-                Returns:
-                    True if successful, False otherwise.
-
-                """
-        # todo Finish docstring
-        resp = self.client.get(reverse('music:index'))
-        self.assertEqual(resp.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        """docstring
-
-                Note:
-                    Do not include the `self` parameter in the ``Args`` section.
-
-                Args:
-                    param1: The first parameter.
-                    param2: The second parameter.
-
-                Returns:
-                    True if successful, False otherwise.
-
-                """
-        # todo Finish docstring
-        resp = self.client.get(reverse('music:index'))
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, 'music/index.html')
-
-    def test_home_view_status_code(self):
-        """docstring
-
-                Note:
-                    Do not include the `self` parameter in the ``Args`` section.
-
-                Args:
-                    param1: The first parameter.
-                    param2: The second parameter.
-
-                Returns:
-                    True if successful, False otherwise.
-
-                """
-        # todo Finish docstring
-        url = reverse('music:index')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
-
-    def test_home_url_resolves_home_view(self):
-        """docstring
-
-                Note:
-                    Do not include the `self` parameter in the ``Args`` section.
-
-                Args:
-                    param1: The first parameter.
-                    param2: The second parameter.
-
-                Returns:
-                    True if successful, False otherwise.
-
-                """
-        # todo Finish docstring
-        view = resolve('/music/')
-        self.assertEquals(view.func, IndexView())
-
-class ModelTests(TestCase):
-    """docstring
-
-        Properties created with the ``@property`` decorator should be documented
-        in the property's getter method.
-
-        Attributes:
-            attr1 (str): Description of `attr1`.
-            attr2 (:obj:`int`, optional): Description of `attr2`.
-
-        """
-    # todo Finish docstring
-    def setUp(self):
-        """docstring
-
-                Note:
-                    Do not include the `self` parameter in the ``Args`` section.
-
-                Args:
-                    param1: The first parameter.
-                    param2: The second parameter.
-
-                Returns:
-                    True if successful, False otherwise.
-
-                """
-        # todo Finish docstring
-        Album.objects.create(name='Django', description='Django board.')
-
-    def test_board_topics_view_success_status_code(self):
-        """docstring
-
-                Note:
-                    Do not include the `self` parameter in the ``Args`` section.
-
-                Args:
-                    param1: The first parameter.
-                    param2: The second parameter.
-
-                Returns:
-                    True if successful, False otherwise.
-
-                """
-        # todo Finish docstring
-        url = reverse('board_topics', kwargs={'pk': 1})
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
-
-    def test_board_topics_view_not_found_status_code(self):
-        """docstring
-
-                Note:
-                    Do not include the `self` parameter in the ``Args`` section.
-
-                Args:
-                    param1: The first parameter.
-                    param2: The second parameter.
-
-                Returns:
-                    True if successful, False otherwise.
-
-                """
-        # todo Finish docstring
-        url = reverse('board_topics', kwargs={'pk': 99})
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 404)
-
-    def test_board_topics_url_resolves_board_topics_view(self):
-        """docstring
-
-                Note:
-                    Do not include the `self` parameter in the ``Args`` section.
-
-                Args:
-                    param1: The first parameter.
-                    param2: The second parameter.
-
-                Returns:
-                    True if successful, False otherwise.
-
-                """
-        # todo Finish docstring
-        view = resolve('/boards/1/')
-        self.assertEquals(view.func, board_topics)
+#
+# Attributes:
+#     module_level_variable1 (int): Module level variables may be documented in
+#         either the ``Attributes`` section of the module docstring, or in an
+#         inline docstring immediately following the variable.
+#
+# Todo:
+#
+# """
+# # todo Finish docstring
+#
+# from django.core.urlresolvers import reverse
+# from django.urls import resolve
+# from django.test import TestCase
+# from .views.views import *
+# from .models import *
+#
+# #
+# # class PageTests(TestCase):
+# #     """docstring
+# #
+# #         Properties created with the ``@property`` decorator should be documented
+# #         in the property's getter method.
+# #
+# #         Attributes:
+# #             attr1 (str): Description of `attr1`.
+# #             attr2 (:obj:`int`, optional): Description of `attr2`.
+# #
+# #         """
+# #     # todo Finish docstring
+# #
+# #     def setUp(self):
+# #         """docstring
+# #
+# #                 Note:
+# #                     Do not include the `self` parameter in the ``Args`` section.
+# #
+# #                 Args:
+# #                     param1: The first parameter.
+# #                     param2: The second parameter.
+# #
+# #                 Returns:
+# #                     True if successful, False otherwise.
+# #
+# #                 """
+# #         # todo Finish docstring
+# #         # Setup run before every test method.
+# #         pass
+# #
+# #     def tearDown(self):
+# #         """docstring
+# #
+# #                 Note:
+# #                     Do not include the `self` parameter in the ``Args`` section.
+# #
+# #                 Args:
+# #                     param1: The first parameter.
+# #                     param2: The second parameter.
+# #
+# #                 Returns:
+# #                     True if successful, False otherwise.
+# #
+# #                 """
+# #         # todo Finish docstring
+# #         # Clean up run after every test method.
+# #         pass
+# #
+# #     def test_view_url_exists_at_desired_location(self):
+# #         """docstring
+# #
+# #                 Note:
+# #                     Do not include the `self` parameter in the ``Args`` section.
+# #
+# #                 Args:
+# #                     param1: The first parameter.
+# #                     param2: The second parameter.
+# #
+# #                 Returns:
+# #                     True if successful, False otherwise.
+# #
+# #                 """
+# #         # todo Finish docstring
+# #         resp = self.client.get('/music')
+# #         self.assertEqual(resp.status_code, 200)
+# #
+# #     def test_view_url_accessible_by_name(self):
+# #         """docstring
+# #
+# #                 Note:
+# #                     Do not include the `self` parameter in the ``Args`` section.
+# #
+# #                 Args:
+# #                     param1: The first parameter.
+# #                     param2: The second parameter.
+# #
+# #                 Returns:
+# #                     True if successful, False otherwise.
+# #
+# #                 """
+# #         # todo Finish docstring
+# #         resp = self.client.get(reverse('music:index'))
+# #         self.assertEqual(resp.status_code, 200)
+# #
+# #     def test_view_uses_correct_template(self):
+# #         """docstring
+# #
+# #                 Note:
+# #                     Do not include the `self` parameter in the ``Args`` section.
+# #
+# #                 Args:
+# #                     param1: The first parameter.
+# #                     param2: The second parameter.
+# #
+# #                 Returns:
+# #                     True if successful, False otherwise.
+# #
+# #                 """
+# #         # todo Finish docstring
+# #         resp = self.client.get(reverse('music:index'))
+# #         self.assertEqual(resp.status_code, 200)
+# #         self.assertTemplateUsed(resp, 'music/index.html')
+# #
+# #     def test_home_view_status_code(self):
+# #         """docstring
+# #
+# #                 Note:
+# #                     Do not include the `self` parameter in the ``Args`` section.
+# #
+# #                 Args:
+# #                     param1: The first parameter.
+# #                     param2: The second parameter.
+# #
+# #                 Returns:
+# #                     True if successful, False otherwise.
+# #
+# #                 """
+# #         # todo Finish docstring
+# #         url = reverse('music:index')
+# #         response = self.client.get(url)
+# #         self.assertEquals(response.status_code, 200)
+# #
+# #     def test_home_url_resolves_home_view(self):
+# #         """docstring
+# #
+# #                 Note:
+# #                     Do not include the `self` parameter in the ``Args`` section.
+# #
+# #                 Args:
+# #                     param1: The first parameter.
+# #                     param2: The second parameter.
+# #
+# #                 Returns:
+# #                     True if successful, False otherwise.
+# #
+# #                 """
+# #         # todo Finish docstring
+# #         view = resolve('/music/')
+# #         self.assertEquals(view.func, IndexView())
+# #
+# # class ModelTests(TestCase):
+# #     """docstring
+# #
+# #         Properties created with the ``@property`` decorator should be documented
+# #         in the property's getter method.
+# #
+# #         Attributes:
+# #             attr1 (str): Description of `attr1`.
+# #             attr2 (:obj:`int`, optional): Description of `attr2`.
+# #
+# #         """
+# #     # todo Finish docstring
+# #     def setUp(self):
+# #         """docstring
+# #
+# #                 Note:
+# #                     Do not include the `self` parameter in the ``Args`` section.
+# #
+# #                 Args:
+# #                     param1: The first parameter.
+# #                     param2: The second parameter.
+# #
+# #                 Returns:
+# #                     True if successful, False otherwise.
+# #
+# #                 """
+# #         # todo Finish docstring
+# #         Album.objects.create(name='Django', description='Django board.')
+# #
+# #     def test_board_topics_view_success_status_code(self):
+# #         """docstring
+# #
+# #                 Note:
+# #                     Do not include the `self` parameter in the ``Args`` section.
+# #
+# #                 Args:
+# #                     param1: The first parameter.
+# #                     param2: The second parameter.
+# #
+# #                 Returns:
+# #                     True if successful, False otherwise.
+# #
+# #                 """
+# #         # todo Finish docstring
+# #         url = reverse('board_topics', kwargs={'pk': 1})
+# #         response = self.client.get(url)
+# #         self.assertEquals(response.status_code, 200)
+# #
+# #     def test_board_topics_view_not_found_status_code(self):
+# #         """docstring
+# #
+# #                 Note:
+# #                     Do not include the `self` parameter in the ``Args`` section.
+# #
+# #                 Args:
+# #                     param1: The first parameter.
+# #                     param2: The second parameter.
+# #
+# #                 Returns:
+# #                     True if successful, False otherwise.
+# #
+# #                 """
+# #         # todo Finish docstring
+# #         url = reverse('board_topics', kwargs={'pk': 99})
+# #         response = self.client.get(url)
+# #         self.assertEquals(response.status_code, 404)
+# #
+# #     def test_board_topics_url_resolves_board_topics_view(self):
+# #         """docstring
+# #
+# #                 Note:
+# #                     Do not include the `self` parameter in the ``Args`` section.
+# #
+# #                 Args:
+# #                     param1: The first parameter.
+# #                     param2: The second parameter.
+# #
+# #                 Returns:
+# #                     True if successful, False otherwise.
+# #
+# #                 """
+# #         # todo Finish docstring
+# #         view = resolve('/boards/1/')
+# #         self.assertEquals(view.func, board_topics)
